0567823fa010fbed52cb53fdaf843a32e540150b
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-01 23:58:13 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -19,8 +21,29 @@ app.get('/users/:id',function(req,res){
 	res.json({
 		success: true,
 		message: 'got one user',
-		user: req.params.id
+		user: mockUserData[req.params.id],
 	})
+})
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
 
 app.listen(8000,function(){
