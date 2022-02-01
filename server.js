732b07c49ec1d2904a26a7d1d29c242519a996a4
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-01 23:55:05 +0100

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/users', function(req,res){
         })
 });
 
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
 app.listen(8000,function(){
     console.log("server is running")
 });
